258e20659650d3822db269b12dbd8bf732881c5b
rafaborgesg-ui <rafaborgesg-ui@users.noreply.github.com>
2026-05-24 03:33:21 -0300

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from '@/App.jsx'
 import '@/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <>
+    <App />
+    <Analytics />
+  </>
 )
